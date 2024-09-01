73bda7a3c5f0cb535744c612d6ae05b4ba6a4e6f
Robotic Forest <roboticforest@gmail.com>
2024-09-01 18:28:48 -0500

    Create simple Hello World server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+
+server.get("/", function (req, res) {
+    let body = "<h1>HOME PAGE</h1>";
+    body += "<a href='/hello'>Hello.</a>";
+    res.send(body);
+});
+
+server.get("/hello", function (req, res) {
+    let body = "<h1>Hello World!</h1>";
+    body += "<p>Greetings.</p>";
+    body += "<p>Warmest regards.</p>";
+    res.send(body);
+});
+
+server.listen(port, function () {
+    console.log("Listening on " + port);
+});
