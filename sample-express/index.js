1be2915694e145f160269b6410c198ca13dc896e
Robotic Forest <roboticforest@gmail.com>
2024-09-01 19:01:28 -0500

    Update and Add routes.

--- sample-express/index.js
+++ sample-express/index.js
@@ -11,10 +11,22 @@ server.get("/", function (req, res) {
 });
 
 server.get("/hello", function (req, res) {
-    let body = "<h1>Hello World!</h1>";
-    body += "<p>Greetings.</p>";
-    body += "<p>Warmest regards.</p>";
-    res.send(body);
+    // let body = "<h1>Hello World!</h1>";
+    // body += "<p>Greetings.</p>";
+    // body += "<p>Warmest regards.</p>";
+    // res.send(body);
+    const response = routeHello(req, res);
+    res.send(response);
+});
+
+server.get("/api/names", async function (req, res) {
+    let response;
+    try {
+        response = await routeAPINames(requ, res);
+    } catch (error) {
+        console.log(error);
+    }
+    res.send(response);
 });
 
 server.listen(port, function () {
